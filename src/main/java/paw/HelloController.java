1aa180ca58738ca248f2e7584105952170e2cc36
jaredgreen <jgreen2@pillartechnology.com>
2019-02-19 09:46:49 -0700

    Added a HelloService and HelloController to return HelloWorld on the base endpoint

---
+++ src/main/java/paw/HelloController.java
@@ -0,0 +1,19 @@
+package paw;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private HelloService helloService;
+
+    @RequestMapping("/")
+    public String helloWorld() {
+        return helloService.retrieveHelloWorldStatement();
+    }
+
+}
--- src/main/java/paw/HelloService.java
+++ src/main/java/paw/HelloService.java
@@ -1,9 +1,12 @@
 package paw;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloService {
 
     public String retrieveHelloWorldStatement() {
         return "Hello World!";
     }
-    
+
 }
